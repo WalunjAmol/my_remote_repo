bf32b0269ee40a68c87223fb179152146fbb62a8
walunjamol <walunjamol444@gmail.com>
2021-11-26 14:14:38 +0530

    Second Commit

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-fir i in range(10):
-	prinnt('i love the Django For providing this facilities or fuction for me')
+print("#"*65)
+for i in range(10):
+	print('i love the Django For providing this facilities or fuction for me')
+print("#"*65)
